a54b86dd0d332b10962416fd23ce36ba92c7037e
Christopher Thielen <cthielen@gmail.com>
2014-03-11 12:59:52 -0700

    Disable SqlBypass data store in attempt to fix session issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 #Rails.application.config.session_store :cookie_store, key: '_devboard_session'
 Rails.application.config.session_store :active_record_store
-ActionDispatch::Session::ActiveRecordStore.session_class = ActiveRecord::SessionStore::SqlBypass
+#ActionDispatch::Session::ActiveRecordStore.session_class = ActiveRecord::SessionStore::SqlBypass
