878451586b62eb754f4aa25b1c5fa11e01b7922d
Christopher Thielen <cthielen@gmail.com>
2014-03-11 13:00:26 -0700

    Deployment update

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
-    run "mkdir -p #{current_path}/tmp/sessions/"
+    run "mkdir -p #{current_path}/tmp/sessions"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
