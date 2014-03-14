0f7a18a84a03030551e845dc37befd013ce1affb
Christopher Thielen <cthielen@gmail.com>
2014-03-14 15:13:31 -0700

    tmp/sessions is no longer required

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,6 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
-    run "mkdir -p #{release_path}/tmp/sessions"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
