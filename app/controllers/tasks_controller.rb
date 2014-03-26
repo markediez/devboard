cd9242a86cddd540e322023d8b2d559cb56bbf6d
Christopher Thielen <cthielen@gmail.com>
2014-03-26 10:52:42 -0700

    ActivityLog now tracks when tasks are edited or completed (regardless of GitHub status)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
   # GET /tasks/new
   def new
     @task = Task.new
-    
+
     if params[:project_id] and (Project.find_by_id(params[:project_id]) != nil)
       @task.project_id = params[:project_id]
     else
@@ -40,14 +40,14 @@ class TasksController < ApplicationController
       if @task.save
         notice_txt = 'Task was successfully created.'
         notice_txt += " GitHub issue ##{@task.gh_issue_number}." if @task.gh_issue_number
-        
+
         ActivityLog.create!({developer_id: current_user.developer_id, project_id: @task.project_id, task_id: @task.id, activity_type: :created })
-        
+
         format.html { redirect_to @task.project, notice: notice_txt }
         format.json { render action: 'show', status: :created, location: @task }
       else
         logger.debug @task.errors.full_messages
-        
+
         format.html { render action: 'new' }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
@@ -58,25 +58,31 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1.json
   def update
     completed_val_before = @task.completed
-    
+
     respond_to do |format|
       if @task.update(task_params)
-        
+
+        # Was this task completed or merely updated?
         # AR Dirty will clear @task.changes in the above update but we need to do this
         # after the update in order to know that the update was successful.
-        if @task.gh_issue_number and (completed_val_before != @task.completed) and not @task.completed.blank?
-          Rails.logger.debug "Closing a GitHub issue ..."
-          close_github_issue
+        if(completed_val_before != @task.completed) and not @task.completed.blank?
           ActivityLog.create!({developer_id: current_user.developer_id, project_id: @task.project_id, task_id: @task.id, activity_type: :completed })
+
+          if @task.gh_issue_number
+            Rails.logger.debug "Closing a GitHub issue ..."
+            close_github_issue
+          else
+            Rails.logger.debug "Not closing a GitHub issue."
+          end
         else
-          Rails.logger.debug "Not closing a GitHub issue."
+          ActivityLog.create!({developer_id: current_user.developer_id, project_id: @task.project_id, task_id: @task.id, activity_type: :edited })
         end
-        
+
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { head :no_content }
       else
         logger.debug @task.errors.full_messages
-        
+
         format.html { render action: 'edit' }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
@@ -103,30 +109,30 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:title, :details, :developer_id, :project_id, :completed, :difficulty, :duration, :due, :priority)
     end
-    
+
     # Uses the GitHub API to create a GitHub issue
     def create_github_issue
       github = Github.new oauth_token: current_user.developer.gh_personal_token
-      
+
       ret = github.issues.create @task.project.gh_repo_url_parse(:user), @task.project.gh_repo_url_parse(:project),
       {
         "title" => @task.title,
         "body" => @task.details
         #"assignee" => current_user.developer.gh_username
       }
-      
+
       @task.gh_issue_number = ret[:number]
     end
-    
+
     # Uses the GitHub API to close a GitHub issue
     def close_github_issue
       github = Github.new oauth_token: current_user.developer.gh_personal_token
-      
+
       ret = github.issues.edit @task.project.gh_repo_url_parse(:user), @task.project.gh_repo_url_parse(:project), @task.gh_issue_number,
       {
         "state" => 'closed'
       }
-      
+
       @gh_closed_at = ret[:closed_at]
     end
 end
