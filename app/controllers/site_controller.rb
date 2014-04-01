96915e0c04eab9531696f506eca13ea63f7a60b4
Christopher Thielen <cthielen@gmail.com>
2014-04-01 15:51:46 -0700

    Order Past Due and Due Soon tasks. Move their calculation logic into the controller.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,21 +1,23 @@
 class SiteController < ApplicationController
   skip_before_filter :authenticate, :only => [:access_denied, :logout]
   skip_before_action :verify_authenticity_token, only: :credentials
-  
+
   # GET /overview
   def overview
     @developers = Developer.order(created_at: :desc)
     @activities = ActivityLog.order(when: :desc).limit(15)
-    
+    @past_due_tasks = Task.where(completed: nil).where('due < ?', DateTime.now).order(due: :asc)
+    @due_soon_tasks = Task.where(completed: nil).where('due < ?', DateTime.now + 10.days).where('due > ?', DateTime.now).order(due: :asc)
+
     authorize! :manage, @developers
     authorize! :manage, @activity
   end
-  
+
   # GET /access_denied
   # Unauthenticated requests are redirected here
   def access_denied
   end
-  
+
   def logout
     CASClient::Frameworks::Rails::Filter.logout(self)
   end
