9007e44266bf55c1d9adc3ee759ba60a3bc11061
Christopher Thielen <cmthielen@ucdavis.edu>
2015-06-17 10:33:03 -0700

    Disable broken migration

--- db/migrate/20150519192504_remove_developer_avatar_fields.rb
+++ db/migrate/20150519192504_remove_developer_avatar_fields.rb
@@ -1,8 +1,8 @@
 class RemoveDeveloperAvatarFields < ActiveRecord::Migration
   def change
-    remove_column :developers, :avatar_file_name
-    remove_column :developers, :avatar_file_type
-    remove_column :developers, :avatar_file_size
-    remove_column :developers, :avatar_uploaded_at
+    # remove_column :developers, :avatar_file_name
+    # remove_column :developers, :avatar_file_type
+    # remove_column :developers, :avatar_file_size
+    # remove_column :developers, :avatar_uploaded_at
   end
 end
