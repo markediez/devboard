eb58c8d148bb6955d88aa6fd4ef26120a5146edc
Christopher Thielen <cmthielen@ucdavis.edu>
2016-04-18 20:50:27 -0700

    Sync commits once per hour.

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,9 +17,6 @@ job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
 every 1.hour do
   envcommand 'nice -n 10 bundle exec rake github:sync_tasks --silent >/dev/null 2>&1'
   envcommand 'nice -n 10 bundle exec rake github:sync_milestones --silent >/dev/null 2>&1'
-end
-
-every 1.day, :at => '4:45 am' do
   envcommand 'nice -n 10 bundle exec rake github:sync_commits --silent >/dev/null 2>&1'
 end
 
