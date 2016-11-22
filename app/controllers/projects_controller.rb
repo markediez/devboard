857485527e00fcd6970bd2ccaa88198ae5a3ccf6
Mark Diez <markediez@gmail.com>
2016-11-22 13:37:07 -0800

    Whitelist all params under project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -80,6 +80,10 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :status, :began, :finished, :priority, :link, :gh_repo_url, :description, :due)
+      # params.require(:project).permit(:name, :status, :began, :finished, :priority, :link, :description, :due, repositories_attributes: [new_repositories: [:gh_url, :_destroy] ])
+
+      # TODO: Figure out how to white list everything in repositories_attributes
+      # "everything" because on update repositories_attributes {"0" => "gh_url=> , _destory=>, id=>"  ... }
+      params.require(:project).permit!
     end
 end
