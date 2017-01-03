39111cd7d4a691272ed065c953263275094da7b0
Mark Diez <markediez@gmail.com>
2017-01-03 12:52:21 -0800

    Refactor helper ot application_helper to reduce redundancy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
   include Authentication
-  
+
   def authenticated?
     not current_user.nil?
+  end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+
+    link_to name, "#", onclick: "return rails_nested_form_add_fields(\"#{association}\", \"#{escape_javascript(fields)}\"); ", class: "add-field"
   end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,14 +2,4 @@ module ProjectsHelper
   def project_status_in_words(status)
     return status.to_s.gsub(/_/, ' ').capitalize
   end
-
-  # Used to add more repository fields
-  def link_to_add_fields(name, f, association)
-    new_object = Repository.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render("repository_fields", :f => builder)
-    end
-
-    link_to name, "#", onclick: "return add_fields(\"#{association}\", \"#{escape_javascript(fields)}\"); ", class: "add-field"
-  end
 end
