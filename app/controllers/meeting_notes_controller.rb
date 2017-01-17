182d116e1207ba5eee3ac5a44b1dec9b57ce89d7
Christopher Thielen <cmthielen@ucdavis.edu>
2017-01-17 13:45:32 -0800

    Fix tests.

--- app/controllers/meeting_notes_controller.rb
+++ app/controllers/meeting_notes_controller.rb
@@ -80,7 +80,7 @@ class MeetingNotesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_meeting_note
       meeting_note_id = params[:id].to_i
-      @meeting_note = MeetingNote.find(id: meeting_note_id)
+      @meeting_note = MeetingNote.find_by(id: meeting_note_id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
