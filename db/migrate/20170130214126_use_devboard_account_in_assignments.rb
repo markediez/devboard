57f3537d56919e4ce30a31ef5a056c7a3de001b6
Mark Diez <markediez@gmail.com>
2017-01-30 15:07:27 -0800

    Fix migration to check if assignment has developers

--- db/migrate/20170130214126_use_devboard_account_in_assignments.rb
+++ db/migrate/20170130214126_use_devboard_account_in_assignments.rb
@@ -1,8 +1,10 @@
 class UseDevboardAccountInAssignments < ActiveRecord::Migration[5.0]
   def change
     Assignment.all.each do |a|
-      a.developer_account_id = a.developer.devboard_account_id
-      a.save!
+      if a.developer.present?
+        a.developer_account_id = a.developer.devboard_account_id
+        a.save!
+      end
     end
   end
 end
