ee19651dacac80b7b5646ac7f8ee0d9fbceb9d06
Christopher Thielen <cmthielen@ucdavis.edu>
2017-02-06 10:41:38 -0800

    Only show active developers in the assignments area.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -4,7 +4,7 @@ class AssignmentsController < ApplicationController
     @tasks = Task.where.not(:id => Assignment.select(:task_id).uniq).where(:completed_at => nil).order(:sort_position => "ASC")
     @time_to_view = params[:time_in_seconds].present? ? Time.at(params[:time_in_seconds].to_i) : Time.now
 
-    @developers = Developer.order(created_at: :desc)
+    @developers = Developer.where(:active => true).order(created_at: :desc)
 
     authorize! :manage, @developers
     authorize! :manage, @activity
