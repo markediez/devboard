669a8362c820f2f1ed0bcd0b62141c756c8e69c4
Christopher Thielen <cmthielen@ucdavis.edu>
2017-02-08 17:08:07 -0800

    Ensure GitHub-linked tasks can only be assigned to GitHub-linked developer accounts.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,6 +3,8 @@ class Assignment < ActiveRecord::Base
   belongs_to :task
   before_create :set_assigned_at
 
+  validate :github_task_must_belong_to_github_account
+
   def developer
     developer_account.present? ? developer_account.developer : nil
   end
@@ -21,6 +23,14 @@ class Assignment < ActiveRecord::Base
   def set_assigned_at
     if assigned_at == nil
       assigned_at = Time.now
+    end
+  end
+
+  def github_task_must_belong_to_github_account
+    if developer_account.present? and task.gh_issue_number > 0
+      if developer_account.account_type != 'github'
+        errors.add "GitHub-linked task can only be assigned to a GitHub-linked developer account."
+      end
     end
   end
 end
