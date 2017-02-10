fe2fcd6808eb89395fb9b587789780ece9956155
shanglu9425 <shanglu9425ll@hotmail.com>
2017-02-10 09:40:45 -0800

    added collapse sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,49 +4,55 @@
 //= require toastr
 
 $(document).ready(function() {
-  $.ajaxSetup({
-    headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') }
-  });
+    $.ajaxSetup({
+        headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') }
+    });
 
-  // Configure the Toastr (plug it in, hope there's butter in the fridge, etc.)
-  toastr.options = {
-    "closeButton": false,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": false,
-    "positionClass": "toast-bottom-center",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-  }
+    // Configure the Toastr (plug it in, hope there's butter in the fridge, etc.)
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    $('[data-toggle=offcanvas]').click(function() {
+        $('.row-offcanvas').toggleClass('active');
+        $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+        $('.badge').toggleClass("badge-test");
+    });
 });
 
 /**
  * Returns the date as a string in the format YYYY-MM-DD HH:MM:SS
  */
 function getFormattedDate(timeInMills) {
-  if(timeInMills === undefined) {
-    timeInMills = Date.now();
-  }
-  var t = new Date(timeInMills);
+    if (timeInMills === undefined) {
+        timeInMills = Date.now();
+    }
+    var t = new Date(timeInMills);
 
-  var year = t.getFullYear();
-  // .slice(-2) ensures 2 digit numbers
-  var month = ("0" + (t.getMonth() + 1) ).slice(-2);
-  var day = ("0" + t.getDate()).slice(-2);
+    var year = t.getFullYear();
+    // .slice(-2) ensures 2 digit numbers
+    var month = ("0" + (t.getMonth() + 1)).slice(-2);
+    var day = ("0" + t.getDate()).slice(-2);
 
-  var hour = ("0" + t.getHours()).slice(-2);
-  var minute = ("0" + t.getMinutes()).slice(-2);
-  var second = ("0" + t.getSeconds()).slice(-2);
+    var hour = ("0" + t.getHours()).slice(-2);
+    var minute = ("0" + t.getMinutes()).slice(-2);
+    var second = ("0" + t.getSeconds()).slice(-2);
 
-  return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 }
 
 /**
@@ -55,10 +61,10 @@ function getFormattedDate(timeInMills) {
  * @param content - html to render
  */
 function rails_nested_form_add_fields(association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
-  $(content.replace(regexp, new_id)).insertBefore(".add-field");
-  return false;
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g")
+    $(content.replace(regexp, new_id)).insertBefore(".add-field");
+    return false;
 }
 
 /**
@@ -67,7 +73,7 @@ function rails_nested_form_add_fields(association, content) {
  * @param link - the button / link clicked by user
  */
 function rails_nested_form_remove_fields(link) {
-  $(link).prev('input[type=hidden]').val("true");
-  $(link).closest('.fields').hide();
-  return false;
+    $(link).prev('input[type=hidden]').val("true");
+    $(link).closest('.fields').hide();
+    return false;
 }
